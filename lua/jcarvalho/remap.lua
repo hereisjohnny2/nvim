95a14c8cd379aa52974a136b068e27e8293353e0
joao.carvalho <joao.carvalho@intelie.com.br>
2023-01-31 14:36:32 -0300

    now the coolest feat ever

--- lua/jcarvalho/remap.lua
+++ lua/jcarvalho/remap.lua
@@ -44,3 +44,6 @@ vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
